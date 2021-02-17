290f3c1891d6669bdf11a6418857310a9f82c10f
Cedric <cedric-lopez@hotmail.fr>
2021-02-17 12:47:37 +0100

    Compétences informatiques icones et cercle

--- portfoliobis/js/index.js
+++ portfoliobis/js/index.js
@@ -36,17 +36,15 @@ function afficher_tous_les_skills(){
 		$(data.competences).each(function(index){
 			//console.log(data.competences[index].deja_etudie);
 			data.competences[index].deja_etudie == 1 ? afficher_un_skill(data, index, competences_acquises): afficher_un_skill(data, index, competences_futures);
-			//data.competences[index].deja_etudie == 1 ? afficher_un_skillbis(data, index, competences_acquises): afficher_un_skillbis(data, index, competences_futures);
-			//data.competences[index].deja_etudie == 1 ? afficher_un_skilltri(data, index, competences_acquises): afficher_un_skilltri(data, index, competences_futures);
-			//data.competences[index].deja_etudie == 1 ? afficher_un_skillq(data, index, competences_acquises): afficher_un_skillq(data, index, competences_futures);
+			data.competences[index].deja_etudie == 1 ? afficher_un_skillbis(data, index, competences_acquises): afficher_un_skillbis(data, index, competences_futures);
+			data.competences[index].deja_etudie == 1 ? afficher_un_skilltri(data, index, competences_acquises): afficher_un_skilltri(data, index, competences_futures);
 		})
 
 	})
 }
 function afficher_un_skill(data, index, division){
-	$(division).append('<data class="un_skill"><img src="'+data.competences[index].image+'"><canvas id="canvas_'+index+'" width="200" height="200"></canvas></data>');
+	$(division).append('<canvas id="canvas_'+index+'" width="200" height="200"></canvas>');
 	var canvas = $('canvas')[index];
-
 	var context = canvas.getContext('2d');
 	if(data.competences[index].maitrise > 70){
 		context.fillStyle = "lime";
@@ -61,10 +59,10 @@ function afficher_un_skill(data, index, division){
 	context.arc(100, 100, 70, 2*Math.PI*(0.25-ratio_maitise), 2*Math.PI*(0.25+ratio_maitise)); 
 	context.fill();
 	console.log(data.competences[index].image);
-	//$(canvas).append('<img src="'+data.competences[index].image+'">');
+	$(canvas).append('<img src="'+data.competences[index].image+'">');
 
 
-	//console.log(data.competences[index].deja_etudie);
+	console.log(data.competences[index].deja_etudie);
 
 	return false;
 }
@@ -79,16 +77,16 @@ function afficher_un_skillbis(data, index, division){
 	}else{
 		context.fillStyle = "red";
 	}
-	var ratio_maitise = (data.competences[index].maitrise/50)-1;
+	var ratio_maitise = data.competences[index].maitrise/200;
 	console.log(ratio_maitise);
 	context.beginPath(); 
-	context.arc(100, 100, 70, 0, 0, Math.asin(ratio_maitise)); 
+	context.arc(100, 100, 70, Math.PI*-0.5, Math.PI*1); 
 	context.fill();
 	console.log(data.competences[index].image);
 	$(canvas).append('<img src="'+data.competences[index].image+'">');
 
 
-	//console.log(data.competences[index].deja_etudie);
+	console.log(data.competences[index].deja_etudie);
 
 	return false;
 }
@@ -103,43 +101,20 @@ function afficher_un_skilltri(data, index, division){
 	}else{
 		context.fillStyle = "red";
 	}
-	var ratio_maitise = data.competences[index].maitrise/100;
+	var ratio_maitise = data.competences[index].maitrise/200;
 	console.log(ratio_maitise);
 	context.beginPath(); 
-	context.arc(100, 100, 70, 0.5*Math.PI, 2*ratio_maitise*Math.PI); 
+	context.arc(100, 100, 70, 2*Math.PI*0, 0); 
 	context.fill();
 	console.log(data.competences[index].image);
 	$(canvas).append('<img src="'+data.competences[index].image+'">');
 
 
-	//console.log(data.competences[index].deja_etudie);
+	console.log(data.competences[index].deja_etudie);
 
 	return false;
 }
-function afficher_un_skillq(data, index, division){
-	$(division).append('<canvas id="canvas_'+index+'" width="200" height="200"></canvas>');
-	var canvas = $('canvas')[index];
-	var context = canvas.getContext('2d');
-	if(data.competences[index].maitrise > 70){
-		context.fillStyle = "lime";
-	}else if(data.competences[index].maitrise >= 50){
-		context.fillStyle = "orange";
-	}else{
-		context.fillStyle = "red";
-	}
-	var ratio_maitise = data.competences[index].maitrise/100;
-	console.log(ratio_maitise);
-	context.beginPath(); 
-	context.arc(100, 100, 70, 0, 0); 
-	context.fill();
-	console.log(data.competences[index].image);
-	$(canvas).append('<img src="'+data.competences[index].image+'">');
 
-
-	//console.log(data.competences[index].deja_etudie);
-
-	return false;
-}
 function XXXXXXXXX(){
 	console.log("test");
 }
